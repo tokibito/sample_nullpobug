81159088173f98e15e1b28e0d60d775cdd737e3f
Shinya Okano <tokibito@gmail.com>
2013-11-04 23:07:04 +0900

    bottleでのファイルダウンロード例

---
+++ python/bottle_file_download/main.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+import csv
+try:
+    # Python2.7ではcsv.writerがunicode対応ではないのでStringIOモジュールを使用する
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from bottle import get, response, run
+
+
+@get('/download')
+def download():
+    """CSVファイルを作成してダウンロードさせる
+    """
+    stream = StringIO()
+    writer = csv.writer(stream)
+    writer.writerow(['foo', '123'])
+    writer.writerow(['bar', '456'])
+    # ストリームの読み書きの位置を先頭に変更する
+    stream.seek(0)
+    # Webブラウザがダウンロードしたファイルを保存するようにヘッダを指定する
+    response.content_type = 'applciation/octet-stream'
+    # ファイル名をtest.csvとする
+    response.headers['Content-Disposition'] = "attachment; filename='test.csv'"
+    return stream.getvalue()
+
+
+if __name__ == '__main__':
+    run(host='127.0.0.1', port=5000)
