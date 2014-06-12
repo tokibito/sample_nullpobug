afddcea4139c5b0a74bb9f9aba9260e5c0e1964e
Shinya Okano <tokibito@gmail.com>
2014-06-12 13:45:28 +0900

    設定変更

--- django/market/market/settings.py
+++ django/market/market/settings.py
@@ -58,8 +58,11 @@ WSGI_APPLICATION = 'market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'market',
+        'USER': 'root',
     }
 }
 
