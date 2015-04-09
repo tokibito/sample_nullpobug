b75a3e3fddfe705cae331b7f54f81129373246b7
Shinya Okano <tokibito@gmail.com>
2015-04-09 23:10:29 +0900

    appをキャッシュするように修正

--- django/django_celery/myproject/myproject/celery_app.py
+++ django/django_celery/myproject/myproject/celery_app.py
@@ -1,5 +1,11 @@
+app = None
+
+
 def get_celery_app():
+    global app
     import celery
+    if app is not None:
+        return app
     app = celery.Celery('myproject')
     app.config_from_object('django.conf:settings')
     return app
