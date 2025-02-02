b29f6157723c91de40a57c4a32627c2a9007155c
Shinya Okano <shinya.okano@open-c.jp>
2025-02-03 02:14:37 +0900

    コメント

--- python/rich_cui_progress/main.py
+++ python/rich_cui_progress/main.py
@@ -1,11 +1,11 @@
 import time
 from rich.progress import track
 
-values = list(range(10))
+values = list(range(10))  # 0～9 の数値のリストを用意
 result = 0
 
 for value in track(values, description="処理中..."):
-    result += value
+    result += value  # 値を足し合わせ
     time.sleep(0.5)  # 意図的に遅延させる
 
-print(result)
+print(result)  # 結果表示
